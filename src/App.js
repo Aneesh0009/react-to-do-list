3abc408b05a5906c41653adbd3be5b76235944f0
Aneesh0009 <aneeshmorampudi05@gmail.com>
2025-08-08 23:40:35 +0530

    Initial commit: Basic todo app without data persistence
    Added the initial version of the React todo app.
    Features:
    - Add and delete todos
    - Simple UI
    
    Does not include localStorage persistence yet.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
 import './App.css';
 
+
 function App() {
+  const [item, setItem] = useState('');
+  const [itemlist, setItemlist] = useState([]);
+
+  const onitemchange = (e) => {
+    setItem(e.target.value);
+  };
+
+  const onadditem = () => {
+    if (item.trim() === '') return; 
+    setItemlist([...itemlist, { id: uuid(), item ,checked : false }]); 
+    setItem('');
+  };
+
+  const oncheckboxchange = (id) => {
+    const updatedList = itemlist.map((items) => {
+      if (items.id === id) {
+        return { ...items, checked: !items.checked }; 
+      }
+      return items;
+    });
+    setItemlist(updatedList);
+  };
+
+  const ondeleteitem = (id) => {
+    const updatedList = itemlist.filter((items) => items.id !== id);
+    setItemlist(updatedList);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My WishList</h1>
+      <div className='search-box'>
+          <input value={item} onChange={onitemchange} type="text" placeholder="Add your WishList........." />
+          <button className='button' onClick={onadditem}>Add</button>
+      </div>
+      <div className='items-list'>
+        {
+          itemlist?.length > 0 && itemlist.map((items) => (
+            <div key={items.id} className='item'>
+              <label>
+                <input onChange={() => oncheckboxchange(items.id)} type="checkbox" />
+                <span className={items.checked ? 'checked' : ''}>{items.item}</span>
+                <button className='button' onClick={() => ondeleteitem(items.id)}>Delete</button>
+              </label>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
